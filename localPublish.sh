95f491f589b176e87ac74e557fd918b58879edaa
Alan Myers <alan@ossftw.com>
2026-07-20 00:05:00 -0400

    Remove all files except client/ and server/

--- localPublish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-echo "Going to build and PUBLISH the OWOP client, press enter to confirm!"
-read
-set -x
-npm run release
-su -c "rsync -vr --delete --max-delete=1 $(pwd)/dist/ /var/www/ourworldofpixels.com/" -
--- upload.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -vru --checksum --delete --max-delete=1 ./dist/ root@ourworldofpixels.com:/var/www/owop/
